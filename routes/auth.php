793338b07773f1b635807a56749c61c870e70532
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-06-29 10:50:37 -0300

    New auth controller

--- routes/auth.php
+++ routes/auth.php
@@ -6,15 +6,24 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisteredUserController::class, 'store'])
+Route::post('/register', [AuthController::class, 'register'])
  ->middleware('guest')
  ->name('register');
 
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+Route::post('/login', [AuthController::class, 'login'])
  ->middleware('guest')
  ->name('login');
+
+// Route::post('/register', [RegisteredUserController::class, 'store'])
+//  ->middleware('guest')
+//  ->name('register');
+
+// Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+//  ->middleware('guest')
+//  ->name('login');
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
  ->middleware('guest')
